4e217e9d4d709d6017ecf3fbebbf176e4359c5a8
Noah Raby <nbr@mit.edu>
2023-06-21 05:25:18 -0400

    cleaner can_spell, better order?

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::time::Instant;
 
-fn main() -> ! {
+fn main() -> () {
     let start = Instant::now();
     // Read the word list into a vector, zip it with the word_power
     // and character count, then sort it by word power, descending
-    let mut words: Vec<(String, f64, [isize; 27])> = read_lines("./words.txt")
+    let mut words: Vec<(String, f64, [isize; 26])> = read_lines("./words.txt")
         .expect("Words file should be in same directory.")
         .collect::<Result<Vec<String>, _>>()
         .unwrap()
@@ -18,7 +18,7 @@ fn main() -> ! {
         .map(|line| {
             let word = String::from(line.trim());
             let power = word_power(&word);
-            let count = char_count(&word);
+            let count = char_count(&word).0;
             (word, power, count)
         })
         .collect();
@@ -42,7 +42,7 @@ fn main() -> ! {
         .iter()
         .map(|w| w.2.clone())
         .reduce(|sum, count| {
-            <[isize; 27]>::try_from(
+            <[isize; 26]>::try_from(
                 sum.iter()
                     .zip(count.iter())
                     .map(|(s, c)| s + c)
@@ -72,7 +72,7 @@ fn main() -> ! {
     let mut count_time = 0;
     let mut find_time = 0;
     let mut rng = thread_rng();
-    for _ in 0..10000 {
+    for _ in 0..1000 {
         let mut test_string: String = String::new();
         (0..16).for_each(|_| {
             let letter = charset[dist.sample(&mut rng)];
@@ -88,7 +88,7 @@ fn main() -> ! {
         let start = Instant::now();
         let _possible_words: Vec<(String, f64)> = words
             .par_iter()
-            .filter(|word| can_spell(&word.2, &input_chars))
+            .filter(|word| can_spell(&word.2, &input_chars.0, input_chars.1))
             .map(|w| (w.0.clone(), w.1.clone()))
             .collect::<Vec<(String, f64)>>()
             .into_iter()
@@ -97,8 +97,8 @@ fn main() -> ! {
             .collect();
         find_time += start.elapsed().as_micros();
     }
-    println!("Word set average count time: {}ns", count_time / 10000);
-    println!("Word set average find time: {}us", find_time / 10000);
+    println!("Word set average count time: {}ns", count_time / 1000);
+    println!("Word set average find time: {}us", find_time / 1000);
 
     // Repeatedly ask for user input, and give the 10
     // best words that can be spelled from that input
@@ -112,7 +112,7 @@ fn main() -> ! {
         let input_chars = char_count(&input);
         let possible_words: Vec<(String, f64)> = words
             .par_iter()
-            .filter(|word| can_spell(&word.2, &input_chars))
+            .filter(|word| can_spell(&word.2, &input_chars.0, input_chars.1))
             .map(|w| (w.0.clone(), w.1.clone()))
             .collect::<Vec<(String, f64)>>()
             .into_iter()
@@ -154,35 +154,61 @@ fn word_power(word: &String) -> f64 {
 
 // Count the number of each letter a word needs to be spelled
 // eisarntolcdupmghbyfvkwzxqj
-fn char_count(word: &String) -> [isize; 27] {
-    let mut chars = [0; 27];
-    let root = 'a' as usize;
+fn char_count(word: &String) -> ([isize; 26], isize) {
+    let mut chars = [0; 26];
+    let mut wildcards = 0;
     let mut letters = word.chars();
     while let Some(letter) = letters.next() {
         let index = match letter {
             '?' => 26,
-            _ => (letter as usize) - root,
+            'e' => 00,
+            'i' => 01,
+            's' => 02,
+            'a' => 03,
+            'r' => 04,
+            'n' => 05,
+            't' => 06,
+            'o' => 07,
+            'l' => 08,
+            'c' => 09,
+            'd' => 10,
+            'u' => 11,
+            'p' => 12,
+            'm' => 13,
+            'g' => 14,
+            'h' => 15,
+            'b' => 16,
+            'y' => 17,
+            'f' => 18,
+            'v' => 19,
+            'k' => 20,
+            'w' => 21,
+            'z' => 22,
+            'x' => 23,
+            'q' => 24,
+            'j' => 25,
+            _ => panic!(),
         };
+        if index == 26 {
+            wildcards += 1;
+        }
         chars[index] += 1;
         // Skips the 'u' after a 'q'
         if letter == 'q' {
             letters.next();
         }
     }
-    chars
+    (chars, wildcards)
 }
 
 // Check if a word can be spelled given an input
-fn can_spell(word: &[isize; 27], input: &[isize; 27]) -> bool {
-    let mut remaining_wildcards = input[26].clone();
+fn can_spell(word: &[isize; 26], input: &[isize; 26], wildcards: isize) -> bool {
+    let mut debt = 0;
     for index in 0..26 {
-        let char_diff: isize = &input[index] - &word[index];
-        if char_diff < 0 {
-            if char_diff + remaining_wildcards < 0 {
-                return false;
-            } else {
-                remaining_wildcards += char_diff;
-            }
+        let diff: isize = &word[index] - &input[index];
+        debt += diff * (diff > 0) as isize;
+        if debt > wildcards {
+            return false;
         }
     }
     true
