dfa6aacc5fa2024126dbd7eae874114ca3430114
Noah Raby <nbr@mit.edu>
2023-12-31 01:28:59 -0600

    Massive overhaul, CLI, readability, performance

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bookworm Adventures 1/2 Word Finding Script
+
+## Purpose
+Find the best words spellable within Bookworm Adventures given a set of letters (and wildcards if in BA2).
+
+## Usage
+Have Rust installed and then do `cargo build --release`.
+You can then call `./target/release/bookworm-solver` or `./target/release/bookworm-solver --use-ba2` as needed.
+
+Copy of Bookworm Adventures not included, but I think you can "find" a copy [here](https://archive.org/details/BookwormAdventuresDeluxe).
--- src/main.rs
+++ src/main.rs
@@ -1,215 +1,133 @@
-use rand::distributions::WeightedIndex;
-use rand::prelude::*;
+use clap::Parser;
 use rayon::prelude::*;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::time::Instant;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, action)]
+    use_ba2: bool,
+}
+
+struct Word {
+    letters: String,
+    power: u8,
+    counts: Counts,
+}
+
+// This is the simplest way of making a HashMap with my own hasher
+// This lets me only check the characters that have values without iterating
+struct Counts {
+    char_keys: Vec<u8>,
+    char_values: [u8; 26],
+    wildcards: u8,
+}
 
 fn main() -> () {
-    let start = Instant::now();
-    // Read the word list into a vector, zip it with the word_power
-    // and character count, then sort it by word power, descending
-    let mut words: Vec<(String, f64, [isize; 26])> = read_lines("./words.txt")
-        .expect("Words file should be in same directory.")
+    let args = Args::parse();
+    let words_file = File::open(Path::new(if args.use_ba2 {
+        "./ba2-words.txt"
+    } else {
+        "./ba1-words.txt"
+    }))
+    .expect("Bookworm Adventures words file not in directory!");
+    let words_reader = io::BufReader::new(words_file);
+    let mut words: Vec<Word> = words_reader
+        .lines()
         .collect::<Result<Vec<String>, _>>()
-        .unwrap()
+        .expect("Failed to read lines from file!")
         .into_par_iter()
         .map(|line| {
-            let word = String::from(line.trim());
-            let power = word_power(&word);
-            let count = char_count(&word).0;
-            (word, power, count)
+            let letters = String::from(line.trim()).replace("qu", "q");
+            let power = word_power(&letters);
+            let counts = char_counts(&letters);
+            Word {
+                letters,
+                power,
+                counts,
+            }
         })
         .collect();
 
     // Sort the list of words from most to least powerful
-    words.par_sort_unstable_by(|a, b| {
-        if a.1 == b.1 {
-            a.0.len().partial_cmp(&b.0.len()).unwrap()
-        } else {
-            b.1.partial_cmp(&a.1).unwrap()
-        }
-    });
+    words.par_sort_unstable_by(|a, b| b.power.cmp(&a.power));
 
-    println!(
-        "Time taken to generate word list: {}ms",
-        start.elapsed().as_millis()
-    );
-
-    // Shows the distribution of characters in the word list
-    let mut char_dist: Vec<(isize, char)> = words
-        .iter()
-        .map(|w| w.2.clone())
-        .reduce(|sum, count| {
-            <[isize; 26]>::try_from(
-                sum.iter()
-                    .zip(count.iter())
-                    .map(|(s, c)| s + c)
-                    .collect::<Vec<isize>>(),
-            )
-            .unwrap()
-        })
-        .unwrap()
-        .into_iter()
-        .zip('a'..='z')
-        .collect::<Vec<(isize, char)>>();
-
-    char_dist.sort_by(|a, b| a.0.cmp(&b.0));
-    let total_chars = char_dist.iter().map(|t| t.0).sum::<isize>();
-    // println!("{:#?}, {}", char_dist, total_chars);
-    // eisarntolcdupmghbyfvkwzxqj
-
-    let weights: [f64; 26] = <[f64; 26]>::try_from(
-        char_dist
-            .iter()
-            .map(|t| (t.0 as f64) / (total_chars as f64))
-            .collect::<Vec<f64>>(),
-    )
-    .unwrap();
-    let charset = <[char; 26]>::try_from(('a'..='z').collect::<Vec<char>>()).unwrap();
-    let dist = WeightedIndex::new(&weights).unwrap();
-    let mut count_time = 0;
-    let mut find_time = 0;
-    let mut rng = thread_rng();
-    for _ in 0..1000 {
-        let mut test_string: String = String::new();
-        (0..16).for_each(|_| {
-            let letter = charset[dist.sample(&mut rng)];
-            match letter {
-                'q' => test_string += "qu",
-                _ => test_string.push(letter),
-            }
-        });
-
-        let start = Instant::now();
-        let input_chars = char_count(&test_string);
-        count_time += start.elapsed().as_nanos();
-        let start = Instant::now();
-        let _possible_words: Vec<(String, f64)> = words
-            .par_iter()
-            .filter(|word| can_spell(&word.2, &input_chars.0, input_chars.1))
-            .map(|w| (w.0.clone(), w.1.clone()))
-            .collect::<Vec<(String, f64)>>()
-            .into_iter()
-            .take(10)
-            .rev()
-            .collect();
-        find_time += start.elapsed().as_micros();
-    }
-    println!("Word set average count time: {}ns", count_time / 1000);
-    println!("Word set average find time: {}us", find_time / 1000);
-
-    // Repeatedly ask for user input, and give the 10
+    // Repeatedly ask for user input, and give the 5
     // best words that can be spelled from that input
     loop {
         let mut input = String::new();
-        println!("Enter the letters you have (all lowercase, 'Qu' == 'qu'):");
+        println!("Enter letters ('Qu' == 'q', wildcards == non-letters, Ctrl-C to stop):");
         std::io::stdin().read_line(&mut input).unwrap();
         input = String::from(input.trim());
 
-        let start = Instant::now();
-        let input_chars = char_count(&input);
-        let possible_words: Vec<(String, f64)> = words
+        let input_counts = char_counts(&input);
+        let possible_words: Vec<(&String, &u8)> = words
             .par_iter()
-            .filter(|word| can_spell(&word.2, &input_chars.0, input_chars.1))
-            .map(|w| (w.0.clone(), w.1.clone()))
-            .collect::<Vec<(String, f64)>>()
+            .filter(|word| can_spell(&word.counts, &input_counts))
+            .map(|word| (&word.letters, &word.power))
+            .collect::<Vec<(&String, &u8)>>()
             .into_iter()
-            .take(10)
-            .rev()
+            .take(5)
             .collect();
-        let time_taken = start.elapsed().as_micros();
-        println!("{:#?}, {}us", possible_words, time_taken);
+        println!("{:?}", possible_words);
     }
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
 
 // Calculate the relative power of a given word
-fn word_power(word: &String) -> f64 {
-    let mut length = 0.0;
-    let mut letters = word.chars();
-    while let Some(letter) = letters.next() {
-        length += match letter {
-            'b' | 'c' | 'f' | 'h' | 'm' | 'p' => 1.25,
-            'v' | 'w' | 'y' => 1.5,
-            'j' | 'k' => 1.75,
-            'x' | 'z' => 2.0,
-            'q' => {
-                letters.next();
-                2.75
+#[inline]
+fn word_power(word: &String) -> u8 {
+    word.as_bytes().into_iter().fold(0, |length, letter| {
+        length
+            + match letter {
+                b'b' | b'c' | b'f' | b'h' | b'm' | b'p' => 5,
+                b'v' | b'w' | b'y' => 6,
+                b'j' | b'k' => 7,
+                b'x' | b'z' => 8,
+                b'q' => 11,
+                _ => 4,
             }
-            _ => 1.0,
-        };
-    }
-    length
+    })
 }
 
 // Count the number of each letter a word needs to be spelled
-// eisarntolcdupmghbyfvkwzxqj
-fn char_count(word: &String) -> ([isize; 26], isize) {
-    let mut chars = [0; 26];
-    let mut wildcards = 0;
-    let mut letters = word.chars();
-    while let Some(letter) = letters.next() {
-        let index = match letter {
-            '?' => 26,
-            'e' => 00,
-            'i' => 01,
-            's' => 02,
-            'a' => 03,
-            'r' => 04,
-            'n' => 05,
-            't' => 06,
-            'o' => 07,
-            'l' => 08,
-            'c' => 09,
-            'd' => 10,
-            'u' => 11,
-            'p' => 12,
-            'm' => 13,
-            'g' => 14,
-            'h' => 15,
-            'b' => 16,
-            'y' => 17,
-            'f' => 18,
-            'v' => 19,
-            'k' => 20,
-            'w' => 21,
-            'z' => 22,
-            'x' => 23,
-            'q' => 24,
-            'j' => 25,
-            _ => panic!(),
-        };
-        if index == 26 {
-            wildcards += 1;
-        }
-        chars[index] += 1;
-        // Skips the 'u' after a 'q'
-        if letter == 'q' {
-            letters.next();
-        }
-    }
-    (chars, wildcards)
+#[inline]
+fn char_counts(word: &String) -> Counts {
+    word.as_bytes().into_iter().fold(
+        Counts {
+            char_keys: Vec::new(),
+            char_values: [0; 26],
+            wildcards: 0,
+        },
+        |mut counts, letter| {
+            let index = (*letter as usize) - ('a' as usize);
+            match index > 25 {
+                false => {
+                    if counts.char_values[index] == 0 {
+                        counts.char_keys.push(*letter);
+                    }
+                    counts.char_values[index] += 1;
+                }
+                true => counts.wildcards += 1,
+            };
+            counts
+        },
+    )
 }
 
 // Check if a word can be spelled given an input
-fn can_spell(word: &[isize; 26], input: &[isize; 26], wildcards: isize) -> bool {
-    let mut debt = 0;
-    for index in 0..26 {
-        let diff: isize = &word[index] - &input[index];
-        debt += diff * (diff > 0) as isize;
-        if debt > wildcards {
-            return false;
-        }
-    }
-    true
+#[inline]
+fn can_spell(word: &Counts, input: &Counts) -> bool {
+    word.char_keys
+        .iter()
+        .fold(Some(input.wildcards), |wildcards, key| {
+            let index = (*key as usize) - ('a' as usize);
+            let debt = word.char_values[index].checked_sub(input.char_values[index]);
+            match debt {
+                None => wildcards,
+                Some(d) => wildcards?.checked_sub(d),
+            }
+        })
+        .is_some()
 }
